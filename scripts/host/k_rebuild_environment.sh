ff4ee28234a261f48e5b2e61b900d43515553de3
Vladyslav Shcherbyna <vshcherbyna@magento.com>
2019-01-30 00:24:57 -0800

    Fix claim name

--- scripts/host/k_rebuild_environment.sh
+++ scripts/host/k_rebuild_environment.sh
@@ -20,11 +20,18 @@ set -e
 
 # TODO: Need to make sure all resources have been successfully deleted before the attempt of recreating them
 sleep 7
-
+enable_nfs="true"
+while getopts 'd' flag; do
+  case "${flag}" in
+    d) enable_nfs="false" ;;
+    *) error "Unexpected option" && exit 1;;
+  esac
+done
 cd "${vagrant_dir}/etc/helm" && helm install \
     --name magento2 \
     --values values.yaml \
     --set global.monolith.volumeHostPath="${vagrant_dir}" \
+    --set global.persistence.nfs.enabled="${enable_nfs}" \
     --set global.checkout.volumeHostPath="${vagrant_dir}" .
 
 # TODO: Waiting for containers to initialize before proceeding
